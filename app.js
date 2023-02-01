64fc8763e978643d63d0b817a83b2e1237579ed8
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-02-01 23:59:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
+const path=require('path')
+
 const express = require("express");
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
-
 const shopRoutes=require("./routes/shop")
+const contactRoutes=require('./routes/contactus')
+
 
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,'Public')))
 
 app.use('/admin',adminRoutes)
 app.use(shopRoutes)
+app.use(contactRoutes)
 
 app.use((req,res,next)=>{
-  res.status(404).send('<h1>Page Not found</h1>')
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 
 app.listen(3000);
