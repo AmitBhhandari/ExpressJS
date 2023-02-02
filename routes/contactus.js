ab737d80c4f5e1306d2aaacc4bb152ba89df81ee
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-02-02 14:59:43 +0530

    using Model views controller

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const rootDir = require("../util/path");
 
-router.get("/contactus", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "contactus.html"));
-});
+const contactController=require('../controllers/contactus')
 
-router.post("/success", (req, res, next) => {
-    res.send('<h1>Form successfuly filled</h1>');
-});
+router.get("/contactus",contactController.getContactus );
+
+router.post("/success",contactController.postContactus );
 
 
 module.exports = router;
